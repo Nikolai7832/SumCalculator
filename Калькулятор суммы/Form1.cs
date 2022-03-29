78910517474ad2ccc81e55a53f226cd0ce0bf137
Николай Стригачев <nikolai7832@yandex.ru>
2022-03-29 20:59:39 +0300

    b = double.Parse(textBox1.Text); замененно на b = double.Parse(textBox2.Text);

--- Калькулятор суммы/Form1.cs
+++ Калькулятор суммы/Form1.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Калькулятор_суммы
 {
     public partial class Form1 : Form
     {
-        double a;
-        double b;
+        double a=0;
+        double b=0;
         public Form1()
         {
             InitializeComponent();
@@ -21,20 +14,23 @@ namespace Калькулятор_суммы
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            textBox3.Text =($"{a + b}" );
+
         }
-private void textBox1_TextChanged(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
             a = double.Parse(textBox1.Text);
+            string v = $"{a + b}";
+            textBox3.Text = v;
         }
 
-private void textBox2_TextChanged(object sender, EventArgs e)
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            b = double.Parse(textBox1.Text);
+            b = double.Parse(textBox2.Text);
+            string v = $"{a + b}";
+            textBox3.Text = v;
         }
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
-          textBox3.Text = ($"{a + b}");
         }
     }
 }
